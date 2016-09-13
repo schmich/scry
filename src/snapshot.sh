f9f2d313941d2a2172f54bbd2fb1cd4080a9abd7
Chris Schmich <schmch@gmail.com>
2016-09-13 05:42:41 -0500

    Limit to 5 download retries before aborting completely.

--- src/snapshot.sh
+++ src/snapshot.sh
@@ -12,22 +12,33 @@ mkdir "$dir" && cd "$dir"
 
 offset=0
 count=0
+retries=5
 while true; do
   url="https://api.twitch.tv/kraken/streams?limit=100&offset=$offset"
   echo "Downloading $url."
   file="${count}.json"
+
   curl -s -o "$file" -H 'Accept: application/vnd.twitchtv.v3+json' "$url"
   if [ $? -ne 0 ]; then
+    retries=$((retries - 1))
+    if [ $retries -eq 0 ]; then
+      echo "Failed to download $url, aborting."
+      exit 1
+    fi
     echo 'Download failed, trying again.'
     sleep 5
     continue
   fi
+
+  retries=5
   offset=$((offset + 80))
   count=$((count + 1))
-  stream_count=`jq '.streams | length' "$file"`
-  if [ $stream_count -eq 0 ]; then
+
+  streams=`jq '.streams | length' "$file"`
+  if [ $streams -eq 0 ]; then
     break
   fi
+
   sleep 0.5
 done
 
