283c38b316dc0d651f8b94a82736994e605b57fd
Chris Schmich <schmch@gmail.com>
2016-09-29 18:49:37 -0500

    Improve import structure. Make small performance fixes.

--- import/import.rb
+++ import/import.rb
@@ -1,6 +1,5 @@
 require 'mysql2'
-require 'json'
-require 'pp'
+require 'oj'
 require 'date'
 require 'set'
 require 'enumerator'
@@ -16,52 +15,40 @@ $client = Mysql2::Client.new(
   flags: Mysql2::Client::MULTI_STATEMENTS | Mysql2::Client::TRANSACTIONS
 )
 
-def e(s)
-  $client.escape(s)
-end
-
 puts 'Connected.'
 
 def timer(what)
   start = Time.now
-  puts "Start: #{what}"
+  print "#{what}..."
   begin
     yield
   ensure
     elapsed = Time.now - start
-    puts "🕒  Elapsed: #{elapsed.round(2)}s"
+    puts "#{elapsed.round(2)}s"
   end
 end
 
-def process_file(channel_ids, stream_ids, file)
+def parse_file(file, game_ids, channels, channel_samples, streams, stream_samples, featured_samples, channel_ids, stream_ids)
   create_game = $client.prepare('INSERT INTO games (name) VALUES (?) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)')
 
-  games = {}
-  channel_infos = []
-  stream_infos = []
-  stream_sample_infos = []
-  channel_sample_infos = []
-  featured_sample_infos = []
-
   content = `xz -c -d "#{file}"`
-  obj = JSON.parse(content)
-  streams = obj['streams']
+  obj = Oj.load(content, mode: :compat, time_format: :ruby)
   featured = obj['featured'] || []
 
-  streams.each do |stream|
+  obj['streams'].each do |stream|
     channel_id = stream['channel']['_id']
     if channel_ids.add(channel_id)
-      channel_infos << {
+      channels << {
         id: channel_id,
-        name: stream['channel']['name'],
-        display_name: stream['channel']['display_name'],
+        name: stream['channel']['name'] || '',
+        display_name: stream['channel']['display_name'] || '',
         created_at: DateTime.parse(stream['channel']['created_at']).to_time
       }
     end
 
     stream_id = stream['_id']
     if stream_ids.add(stream_id)
-      stream_infos << {
+      streams << {
         id: stream_id,
         channel_id: channel_id,
         created_at: DateTime.parse(stream['created_at']).to_time
@@ -69,89 +56,124 @@ def process_file(channel_ids, stream_ids, file)
     end
 
     game_name = stream['game'] || ''
-    game_id = games[game_name]
+    game_id = game_ids[game_name]
     if game_id.nil?
       create_game.execute(game_name)
       game_id = $client.last_id
-      games[game_name] = game_id
+      game_ids[game_name] = game_id
     end
 
-    stream_sample_infos << {
+    stream_samples << {
       stream_id: stream_id,
       timestamp: stream['at'],
       game_id: game_id,
       viewers: stream['viewers'],
       status: stream['channel']['status'] || '',
-      playlist: stream['is_playlist'],
+      playlist: (stream['is_playlist'] || false) ? 1 : 0
     }
 
-    channel_sample_infos << {
+    channel_samples << {
       channel_id: channel_id,
       timestamp: stream['at'],
       followers: stream['channel']['followers'] || 0,
       views: stream['channel']['views'] || 0,
-      partner: stream['channel']['partner'] || false,
+      partner: (stream['channel']['partner'] || false) ? 1 : 0,
       language: stream['channel']['language'] || '',
-      mature: stream['channel']['mature'] || false
+      mature: (stream['channel']['mature'] || false) ? 1 : 0
     }
   end
 
   featured.each do |feature|
-    featured_sample_infos << {
+    featured_samples << {
       stream_id: feature['stream']['_id'],
       timestamp: feature['at'], 
-      title: feature['title'],
-      text: feature['text'],
+      title: feature['title'] || '',
+      text: feature['text'] || '',
       priority: feature['priority'],
-      sponsored: feature['sponsored']
+      sponsored: (feature['sponsored'] || false) ? 1 : 0
     }
   end
+end
 
-  slice_size = 10_000
-
-  puts 'Insert channels.'
-  channel_infos.each_slice(slice_size) do |g|
-    query = 'INSERT IGNORE INTO channels (id, name, display_name, created_at) VALUES '
-    query += g.map { |i|
-      "(#{i[:id]},\"#{e(i[:name])}\",\"#{e(i[:display_name])}\",\"#{i[:created_at]}\")"
-    }.join(',')
-    result = $client.query(query)
+def insert_records(channels, channel_samples, streams, stream_samples, featured_samples)
+  def e(s)
+    $client.escape(s)
   end
 
-  puts 'Insert streams.'
-  stream_infos.each_slice(slice_size) do |g|
-    query = 'INSERT IGNORE INTO streams (id, channel_id, created_at) VALUES '
-    query += g.map { |r|
-      "(#{r[:id]},#{r[:channel_id]},\"#{r[:created_at]}\")"
-    }.join(',')
-    result = $client.query(query)
-  end
+  timer('Insert records') do
+    $client.query('SET foreign_key_checks=0')
+    $client.query('SET unique_checks=0')
+    $client.query("SET sql_log_bin=0")
+    $client.query('START TRANSACTION')
 
-  puts 'Insert stream_samples.'
-  stream_sample_infos.each_slice(slice_size) do |g|
-    query = 'INSERT IGNORE INTO stream_samples (stream_id, timestamp, game_id, viewers, status, playlist) VALUES '
-    query += g.map { |r|
-      "(#{r[:stream_id]},#{r[:timestamp]},#{r[:game_id]},#{r[:viewers]},\"#{e(r[:status])}\",#{r[:playlist]?1:0})"
-    }.join(',')
-    result = $client.query(query)
-  end
+    batch = 5_000
 
-  puts 'Insert channel_samples.'
-  channel_sample_infos.each_slice(slice_size) do |g|
-    query = 'INSERT IGNORE INTO channel_samples (channel_id, timestamp, followers, views, partner, language, mature) VALUES '
-    query += g.map { |r|
-      "(#{r[:channel_id]},#{r[:timestamp]},#{r[:followers]},#{r[:views]},#{r[:partner]?1:0},\"#{e(r[:language])}\",#{r[:mature]?1:0})"
-    }.join(',')
-    result = $client.query(query)
-  end
+    channels.sort_by { |r| r[:id] }.each_slice(batch) do |rs|
+      query = 'INSERT IGNORE INTO channels (id, name, display_name, created_at) VALUES '
+      query += rs.map { |r|
+        "(#{r[:id]},\"#{e(r[:name])}\",\"#{e(r[:display_name])}\",\"#{r[:created_at]}\")"
+      }.join(',')
+      $client.query(query)
+    end
 
-  puts 'Insert featured_samples.'
-  featured_sample_infos.each_slice(slice_size) do |g|
-    query = 'INSERT IGNORE INTO featured_samples (stream_id, timestamp, title, text, priority, sponsored) VALUES '
-    query += g.map { |r|
-      "(#{r[:stream_id]},#{r[:timestamp]},\"#{e(r[:title])}\",\"#{e(r[:text])}\",#{r[:priority]},#{r[:sponsored]})"
-    }.join(',')
-    result = $client.query(query)
+    channel_samples.sort_by { |r| r[:channel_id ]}.each_slice(batch) do |rs|
+      query = 'INSERT IGNORE INTO channel_samples (channel_id, timestamp, followers, views, partner, language, mature) VALUES '
+      query += rs.map { |r|
+        "(#{r[:channel_id]},#{r[:timestamp]},#{r[:followers]},#{r[:views]},#{r[:partner]?1:0},\"#{e(r[:language])}\",#{r[:mature]?1:0})"
+      }.join(',')
+      $client.query(query)
+    end
+
+    streams.sort_by { |r| r[:id] }.each_slice(batch) do |rs|
+      query = 'INSERT IGNORE INTO streams (id, channel_id, created_at) VALUES '
+      query += rs.map { |r|
+        "(#{r[:id]},#{r[:channel_id]},\"#{r[:created_at]}\")"
+      }.join(',')
+      $client.query(query)
+    end
+
+    stream_samples.sort_by { |r| r[:stream_id] }.each_slice(batch) do |rs|
+      query = 'INSERT IGNORE INTO stream_samples (stream_id, timestamp, game_id, viewers, status, playlist) VALUES '
+      query += rs.map { |r|
+        "(#{r[:stream_id]},#{r[:timestamp]},#{r[:game_id]},#{r[:viewers]},\"#{e(r[:status])}\",#{r[:playlist]?1:0})"
+      }.join(',')
+      $client.query(query)
+    end
+
+    featured_samples.sort_by { |r| r[:stream_id] }.each_slice(batch) do |rs|
+      query = 'INSERT IGNORE INTO featured_samples (stream_id, timestamp, title, text, priority, sponsored) VALUES '
+      query += rs.map { |r|
+        "(#{r[:stream_id]},#{r[:timestamp]},\"#{e(r[:title])}\",\"#{e(r[:text])}\",#{r[:priority]},#{r[:sponsored]})"
+      }.join(',')
+      $client.query(query)
+    end
+
+    $client.query('COMMIT')
+  end
+end
+
+def import(dir, resume_from)
+  $client.query("SET SESSION tx_isolation='READ-UNCOMMITTED'")
+
+  channel_ids = Set.new
+  stream_ids = Set.new
+  game_ids = {}
+
+  groups = Dir["#{dir}/*.json.xz"].sort.drop_while { |f| resume_from && (f != resume_from) }.each_slice(2)
+  groups.each do |group|
+    channels = []
+    channel_samples = []
+    streams = []
+    stream_samples = []
+    featured_samples = []
+
+    group.each do |file|
+      timer("Parse #{file}") do
+        parse_file(file, game_ids, channels, channel_samples, streams, stream_samples, featured_samples, channel_ids, stream_ids)
+      end
+    end
+
+    insert_records(channels, channel_samples, streams, stream_samples, featured_samples)
   end
 end
 
@@ -168,25 +190,7 @@ else
   puts "Resuming import from #{resume_from}."
 end
 
-process = false
 base_dir = File.dirname(__FILE__)
 import_dir = File.join(base_dir, dir)
 
-channel_ids = Set.new
-stream_ids = Set.new
-
-Dir["#{import_dir}/*.json.xz"].each do |file|
-  if resume_from
-    process = true if file == resume_from
-    next if !process
-  end
-
-  result = $client.query('SET foreign_key_checks=0')
-  result = $client.query('SET unique_checks=0')
-  result = $client.query('SET autocommit=0')
-  result = $client.query('START TRANSACTION')
-  timer("Processing #{file}") do
-    process_file(channel_ids, stream_ids, file)
-  end
-  result = $client.query('COMMIT')
-end
+import(import_dir, resume_from)
